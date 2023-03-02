288ec8d22f4b6dffbad5daba9992903fe25840bf
Kwon Valerii <wellko24@gmail.com>
2023-03-02 13:59:44 +0600

    added multer.ts

---
+++ api/config.ts
@@ -0,0 +1,10 @@
+import path from "path";
+
+const rootPath = __dirname;
+const config = {
+    rootPath,
+    publicPath: path.join(rootPath, 'public'),
+    db: 'mongodb://localhost/player'
+};
+
+export default config;
